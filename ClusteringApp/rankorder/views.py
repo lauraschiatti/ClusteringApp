621bcb21306956a466280c9d4dadcdffecaf7768
lauricdd <lauri_cdd@hotmail.com>
2016-09-24 11:24:15 -0500

    Validación tipo de archivo (text/plain) subido desde el form. Ignorar archivos guardados en directorio files/ dentro de rank order

--- ClusteringApp/rankorder/views.py
+++ ClusteringApp/rankorder/views.py
@@ -6,20 +6,31 @@ import os
 
 from .forms import UploadFileForm
 
+#@csrf_protect
 def upload_file(request):
     if request.method == 'POST':# if this is a POST request we need to process the form data
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/')
+            file = request.FILES['file']
+            name = file.name
+            content_type = file.content_type
+            size = file.size
+
+            if content_type != 'text/plain':
+                form = UploadFileForm()
+            else:
+                handle_uploaded_file(file)
+                return HttpResponseRedirect('/')
+
     else: # if a GET (or any other method) we'll create a blank form
         form = UploadFileForm()
     return render(request, 'rankorder/index.html', {'form': form})
 
 #save file data on local file
 def handle_uploaded_file(f):
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #path rankorder folder
+    print "f", f
+    """BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #path rankorder folder
     filepath = os.path.join(BASE_DIR, 'files/weakconcepts.txt')
     with open(filepath, 'wb+') as dest:
         for chunk in f.chunks():
-            dest.write(chunk)
+            dest.write(chunk)"""
