76eca68169552dca10a9ac25842636142de4fc5e
lauricdd <lauri_cdd@hotmail.com>
2016-09-25 09:52:59 -0500

    Agregar templates de equipo y acerca de

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,9 +1,14 @@
-/*http://stackoverflow.com/questions/11533542/twitter-bootstrap-add-active-class-to-li*/
-var url = window.location;
-  // Will only work if string in href matches with location
-  $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
 
-  // Will also work for relative and absolute hrefs
-  $('ul.nav a').filter(function() {
-      return this.href == url;
-  }).parent().addClass('active');
+$( document ).ready(function() {
+    /*http://stackoverflow.com/questions/11533542/twitter-bootstrap-add-active-class-to-li*/
+    var url = window.location;
+    console.log("url", url);
+    // Will only work if string in href matches with location
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+
+    // Will also work for relative and absolute hrefs
+    $('ul.nav a').filter(function() {
+        return this.href == url;
+    }).parent().addClass('active');
+});
+
